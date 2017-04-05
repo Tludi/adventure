5216d4b8b4badf3b1727383661f8f6dbf62bd1e7
Tludi <tludi00@gmail.com>
2017-04-05 16:05:39 -0700

    added initial collection view and add button for first view

--- adventure/ViewController.swift
+++ adventure/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+  let baseArray = ["Hello?", "I said, Hello?!", "Is anyone there?", "I can hear breathing but can not see anything."]
+  var pushedArray:[String] = []
+  
+  var counter = 0
+ 
+
+  
+  @IBAction func addLabelButton(_ sender: UIButton) {
+    print(counter)
+    if pushedArray.count != baseArray.count {
+      let pulledString = baseArray[counter]
+      print(counter)
+      print("\(pushedArray.count) - \(baseArray.count)")
+      pushedArray.append(pulledString)
+      counter += 1
+      print(pushedArray.count)
+    } else {
+      print("no more items in base array")
+    }
+    collectionTable.reloadData()
+  }
+  
+  @IBOutlet weak var collectionTable: UICollectionView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    
+    print("collection view loaded")
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
 
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return pushedArray.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+    let baseTextfield = cell.contentView.viewWithTag(1) as! UITextField
+    baseTextfield.text = pushedArray[indexPath.row]
+    
+    
+    cell.backgroundColor = UIColor.gray
+    return cell
+  }
 
 }
 
